c832fdddb74497adb661e244396b0919b78460a9
валерий Ларин <xj48val@gmail.com>
2024-09-26 00:52:39 +0700

    ChangeRegister

--- Interface/View/RegisterPage.xaml.cs
+++ Interface/View/RegisterPage.xaml.cs
@@ -5,7 +5,14 @@ public partial class RegisterPage : ContentPage
 	public RegisterPage()
 	{
 		InitializeComponent();
-	}
+
+    }
+
+    protected override bool OnBackButtonPressed() 
+    {
+        return false;
+    }
+
 
     public async void Register_Click(object sender, EventArgs args)
     {
